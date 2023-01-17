bef306e4cc7ba961cd9420cc0aff51bcbb6f2929
Kruno Tomola Fabro <krunotf@gmail.com>
2023-01-17 09:31:55 +0100

    Save shell note

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -3,3 +3,8 @@ pub mod shard_shell;
 pub mod vec_shell;
 
 // TODO: Test for shells
+
+// TODO: Shellovi za dvostruki rep:
+// TODO: 1. Shell za dugi plitak rep
+// TODO: 2. Shell za kratki dubok rep
+// TODO: 3. Shell koji može priječi iz 1. u 2.
